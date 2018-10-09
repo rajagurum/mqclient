ce76b432c708a91224503d90d556ffb16fc6d554
rajagurum <rajagurum@gmail.com>
2018-10-09 14:20:25 +0530

    MqClient
    Signed-off-by: rajagurum <rajagurum@gmail.com>

---
+++ spring-boot-mq-client/src/main/java/com/boa/mq/SpringBootMqClientApplication.java
@@ -0,0 +1,12 @@
+package com.boa.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMqClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMqClientApplication.class, args);
+	}
+}
